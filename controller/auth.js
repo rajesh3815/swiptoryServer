82ff5ced2ecac39629e888749cff73f002ba2844
Rajesh Kumar <rkraja730@gmail.com>
2024-04-19 15:36:23 +0530

    authentication added

---
+++ controller/auth.js
@@ -0,0 +1,65 @@
+const user = require("../model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const registerUser = async (req, res) => {
+  const { name, password } = req.body;
+  if (!name || !password) {
+    return res.status(400).send("All fields are require");
+  }
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const newUser = new user({
+      name,
+      password: encryptedPassword,
+    });
+    await newUser.save();
+    res.send("user Created Successfully");
+  } catch (error) {
+    res.status(400).send("Error in user Registration");
+    console.log("error from userRegistration:)", error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { name, password } = req.body;
+  const isUserExist = await user.findOne({ name });
+  if (!isUserExist) {
+    return res.status(500).send({
+      message: "user doesnot exist!",
+      status: 500,
+    });
+  }
+  try {
+    const decryptPassword = await bcrypt.compare(
+      password,
+      isUserExist.password
+    );
+    if (!decryptPassword) {
+      return res.status(500).send({
+        message: "password error",
+        status: 400,
+      });
+    } else {
+      const token = jwt.sign(
+        { userId: isUserExist._id },
+        process.env.SECRET_KEY
+      );
+      return res.send({
+        name,
+        status:200,
+        message: "successfully loged-in",
+        token,
+      });
+    }
+  } catch (error) {
+    res.status(400).send({
+      message: "Error in login",
+      status:300,
+    });
+    console.log("Error from userLogin:)",error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const mongoose = require("mongoose");
+const userRouter = require("./routers/auth.routes");
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/user", userRouter);
+
+// for API health checking
+app.get("/api/health", (req, res) => {
+  res.send("API is working fine");
+});
+// ---------------------------------//
+
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(process.env.URL);
+    console.log("DB connected");
+  } catch (error) {
+    console.log("db connection error");
+  }
+  console.log("server is up :)");
+});
